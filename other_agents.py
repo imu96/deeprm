02f31aaaf96999249e922a2df36aab49d359b207
Luke Franceschini <luke.a.franceschini@gmail.com>
2018-03-26 20:53:06 -0400

    Add the paper online knapsack to other agents

--- other_agents.py
+++ other_agents.py
@@ -1,5 +1,33 @@
 import numpy as np
+import parameters
+from math import exp
 
+max_res = 200
+max_density = 20
+min_density = 1
+
+def psi(upper_bound, lower_bound, frac):
+    threshold = ((upper_bound*exp(1)/lower_bound)**frac)*(lower_bound/exp(1))
+    return threshold
+
+def get_kp_action(machine, job_slot, upper_bound, lower_bound):
+    bag = []
+    used_space = 0
+    for i in xrange(len(job_slot.slot)):
+        new_job = job_slot.slot[i]
+        if new_job is None:
+            continue
+        used_space = sum (map (lambda x: x.res_vec[0], machine.running_job))
+        capacity = machine.res_slot
+        if  used_space == capacity:
+            break;
+        if new_job.len + used_space > capacity:
+            continue;
+        frac = used_space*1.0 / capacity
+        psi_i = psi(upper_bound, lower_bound, frac);
+        if new_job.res_vec[0] >=  psi_i:
+            return i
+    return len(job_slot.slot);
 
 def get_packer_action(machine, job_slot):
         align_score = 0
